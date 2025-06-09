534dfd7919c45a1348666eea325d10c6b77ae94f
tarad36 <Tarad36@gmail.com>
2025-06-09 17:38:39 -0400

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   const apiKey = process.env.GEMINI_API_KEY;
 
   const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${apiKey}`,
+    https://generativelanguage.googleapis.com/v1/models/gemini-1.0-pro-latest:generateContent?key=AIzaSyBxwhdGc7BXPC6pbL8cPSOAbFoDW14LUtg,
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
